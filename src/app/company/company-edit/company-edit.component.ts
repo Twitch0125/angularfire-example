1aea325d03331c2a3cb37d7d19a2df10e7277119
Twitch0125 <twitcherc@gmail.com>
2019-08-21 08:24:53 -0600

    company-edit firebase

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Company } from "src/app/models/Company";
 
 @Component({
-  selector: 'app-company-edit',
-  templateUrl: './company-edit.component.html',
-  styleUrls: ['./company-edit.component.scss']
+  selector: "app-company-edit",
+  templateUrl: "./company-edit.component.html",
+  styleUrls: ["./company-edit.component.scss"]
 })
 export class CompanyEditComponent implements OnInit {
+  company$: Observable<Company>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private db: AngularFirestore) {
+    this.company$ = this.db
+      .doc<Company>("companies/iy1PQa0DRHZcys8iJsdC")
+      .valueChanges();
   }
 
+  ngOnInit() {}
 }
