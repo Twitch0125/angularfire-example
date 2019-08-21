4d88bba39442cd80ae888666aa9272aba22c95c3
Twitch0125 <twitcherc@gmail.com>
2019-08-21 08:29:42 -0600

    company service

---
+++ src/app/company/company.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from "@angular/fire/firestore";
+import { Company } from "../models/Company";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class CompanyService {
+  private companyRef: AngularFirestoreDocument<Company>;
+
+  constructor(private db: AngularFirestore) {
+    this.companyRef = this.db.doc<Company>("companies/iy1PQa0DRHZcys8iJsdC");
+  }
+
+  getCompanyObservable(): Observable<Company> {
+    return this.companyRef.valueChanges();
+  }
+
+  saveCompany(company: Company) {
+    this.companyRef.set(company);
+  }
+}
